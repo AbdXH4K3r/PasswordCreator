bef75f6f4f390e9b32519bf0fbc89fcae64b7c93
AbdXSlayer <39466011+AbdXH4K3r@users.noreply.github.com>
2018-10-02 12:54:39 -0400

    CodeSource.py
    A Professional Tool To create passwordlists.

---
+++ .gitignore/CodeSource.py
@@ -0,0 +1,422 @@
+import os 
+import sys 
+import random
+import time 
+os.system('clear')
+time.sleep(0.5)
+print(""""\033[1;31m"
++======================+======================+
+|             CODED BY ABDXSLAYER             |
+|             AS8APPLE@GMAIL.COM	      |   
++======================+======================+""")
+
+time.sleep(1)
+
+os.system('clear')
+how = raw_input("""\033[0;32m"How many letter You want in the password ? : 
+1)3 letters.
+2)4 letters.
+3)5 letters.
+4)6 letters.
+5)7 letters.
+
+99)Exit.
+
+=> """)
+if how=="1":
+	os.system('clear')
+	hown1 = raw_input(""""\033[1;36m"How many letter You want in the password ? : 
+1)3 numbers.
+2)4 numbers.
+3)5 numbers.
+4)6 numbers.
+5)7 numbers.
+
+99)Exit.
+
+=> """)
+	if hown1 =="1":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100,999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s"%(An)
+			print Ans
+
+
+
+	
+	if hown1 =="2":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000,9999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s"%(An)
+			print Ans
+
+
+	if hown1 =="3":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(10000,99999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s"%(An)
+			print Ans
+
+
+	if hown1 =="4":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100000,999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s"%(An)
+			print Ans
+
+
+	if hown1 =="5":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000000,9999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s"%(An)
+			print Ans
+
+if how=="2":
+	os.system('clear')
+	hown2 = raw_input(""""\033[1;36m"How many letter You want in the password ? : 
+1)3 numbers.
+2)4 numbers.
+3)5 numbers.
+4)6 numbers.
+5)7 numbers.
+
+99)Exit.
+
+=> """)
+	if hown2 =="1":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100,999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown2 =="2":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000,9999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown2 =="3":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(10000,99999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown2 =="4":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100000,999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown2 =="5":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000000,9999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s"%(An)
+			print Ans
+
+
+if how=="3":
+	os.system('clear')
+	hown3 = raw_input(""""\033[1;36m"How many letter You want in the password ? : 
+1)3 numbers.
+2)4 numbers.
+3)5 numbers.
+4)6 numbers.
+5)7 numbers.
+
+99)Exit.
+
+=> """)
+	if hown3 =="1":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100,999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown3 =="2":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000,9999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown3 =="3":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(10000,99999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown3 =="4":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100000,999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown3 =="5":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000000,9999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s"%(An)
+			print Ans
+
+if how=="4":
+	os.system('clear')
+	hown4 = raw_input(""""\033[1;36m"How many letter You want in the password ? : 
+1)3 numbers.
+2)4 numbers.
+3)5 numbers.
+4)6 numbers.
+5)7 numbers.
+
+99)Exit.
+
+=> """)
+
+	if hown4 =="1":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100,999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown4 =="2":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000,9999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown4 =="3":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(10000,99999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown4 =="4":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100000,999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown4 =="5":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000000,9999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+
+
+
+if how=="5":
+	os.system('clear')
+	hown5 = raw_input(""""\033[1;36m"How many letter You want in the password ? : 
+1)3 numbers.
+2)4 numbers.
+3)5 numbers.
+4)6 numbers.
+5)7 numbers.
+
+99)Exit.
+
+=> """)
+
+	if hown5 =="1":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100,999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s%s"%(An)
+			print Ans
+	if hown5 =="2":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000,9999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown5 =="3":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(10000,99999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown5 =="4":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(100000,999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s%s"%(An)
+			print Ans
+
+	if hown5 =="5":
+		os.system('clear')
+		time = int(raw_input("How Many Time ? : "))
+		os.system('clear')
+		i = 0
+		for i in range (0,time):
+			i = i+1
+			Alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+			Num = random.randint(1000000,9999999)
+			An = random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),random.choice(Alph),Num
+			Ans = "%s%s%s%s%s%s%s%s"%(An)
+			print Ans
+	
+
